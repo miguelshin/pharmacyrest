0c52255d148b1c119a0074c089518d67c8aa5591
Miguel Ángel Ortiz BErmejo <miguelaob@gmail.com>
2018-12-30 14:42:37 +0100

    Changes

--- auth/src/main/java/com/pharmacy/auth/entities/UserEntity.java
+++ auth/src/main/java/com/pharmacy/auth/entities/UserEntity.java
@@ -1,14 +1,15 @@
 package com.pharmacy.auth.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @NoArgsConstructor
 @AllArgsConstructor
